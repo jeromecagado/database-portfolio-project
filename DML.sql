ca0a4498a3ef15e6846771587992faba6ed1024a
Gabriel-Jean Bertrand <gabrieljeanbertrand@gmail.com>
2024-02-21 18:01:37 -0800

    made the sql consistent

--- DML.sql
+++ DML.sql
@@ -8,7 +8,7 @@
 -- Create
 
 INSERT INTO Developers (developer_name, developer_country, developer_email)
-VALUES ('Developer Name', 'Developer Country', 'email');
+VALUES (:nameInput, :countryInput, :emailInput);
 
 -- Read
 
@@ -24,73 +24,64 @@ WHERE id = :id_selected_from_developer;
 
 -- Delete
 
+DELETE FROM Developers WHERE id = :id_selected_from_developer
+
+
+-- Statments for Sales
+
+-- Create
+
+INSERT INTO Sales (employee_id, customer_id, sale_revenue, sold_date)
+VALUES (:employeeIdInput, customerIdInput, saleRevenueInput);
+
+-- Read
+
+Select * FROM Sales;
+
+-- Update
+
+UPDATE Sales 
+SET employee_id = employeeIdInput , customer_id = :customerIdInput, sale_revenue = :revenueInput, sold_date = :dateInput
+WHERE sale_id = :id_selected_from_sales;
+
+-- Delete
+
+DELETE FROM Sales WHERE id = :id_selected_from_sales;
+
 -- Select Operations.
 
 
 
 
+--Statements for video games
 
--- Get table of sales.
-Select * FROM Sales;
+-- Create
 
--- Get table of videogames.
-SELECT * FROM VideoGames;
+NSERT INTO VideoGames (developer_id, video_game_name, price, quantity)
+VALUES (:developerIdInput, :nameInput,:priceInput, :quantityInput)
+
+-- Read
+SELECT video_game_id, Developers.developer_name as Developer, video_game_name, price, quantity
+ FROM VideoGames
+ INNER JOIN Developers ON Developers.developer_id = VideoGames.developer_id;
+
+-- Update
+
+Update VideoGames 
+SET developer_id = (SELECT developer_id FROM Developers WHERE developer_name = :developerNameInput), video_game_name = :nameInput, price = :priceInput, quantity = :quantityInput
+WHERE id = :id_selected_from_videogames;
+
+-- Delete
+
+DELETE FROM VideoGames WHERE id = :id_selected_from_videogames;
+
+
 
 -- Get all videogames made by a developer. 
 SELECT VideoGames.video_game_name AS title
 FROM Developers
 JOIN Developers ON Videogames.developer_id = developer.id
 WHERE Developers.name = 'Cool Games';
-
-
-
--- Insert Operations.
-
-
--- Insert Sales data.
-INSERT INTO Sales (employee_id, customer_id, sale_revenue, sold_date)
-VALUES ('Employee ID', 'Customer ID', 'Sale Revenue');
-
--- Insert VideoGames data.
-INSERT INTO VideoGames (developer_id, video_game_name, price, quantity)
-VALUES ('Developer ID', 'Videogame name', 'price', 'quantity')
-
-
-
--- Upate Operations
-
--- Update Developer data. 
-Update Developers SET developer_name = 'Updated Name', developer_country = 'Updated Country', developer_email = 'Updated email' WHERE id = 1;
-Update Developers SET developer_name = 'Updated Name', developer_country = 'Updated Country', developer_email = 'Updated email' WHERE id = 2;
-Update Developers SET developer_name = 'Updated Name', developer_country = 'Updated Country', developer_email = 'Updated email' WHERE id = 3;
-
--- Update Sales data.
-Upate Sales SET employee_id = 'Updated employee', customer_id = 'Updated customer', sale_revenue = 'revenue', sold_date = 'dates sold' WHERE id = 1;
-Upate Sales SET employee_id = 'Updated employee', customer_id = 'Updated customer', sale_revenue = 'revenue', sold_date = 'dates sold' WHERE id = 2;
-Upate Sales SET employee_id = 'Updated employee', customer_id = 'Updated customer', sale_revenue = 'revenue', sold_date = 'dates sold' WHERE id = 3;
-
--- Update VideoGames data.
-Update VideoGames SET developer_id = "Updated developer", video_game_name = 'Updated title', price = 'price', quantity = 'quantity' WHERE id = 1;
-Update VideoGames SET developer_id = "Updated developer", video_game_name = 'Updated title', price = 'price', quantity = 'quantity' WHERE id = 2;
-Update VideoGames SET developer_id = "Updated developer", video_game_name = 'Updated title', price = 'price', quantity = 'quantity' WHERE id = 3;
-
-
--- Delete Operations
-
--- Delete Developer data.
-DELETE FROM Developers WHERE id = 1;
-DELETE FROM Developers WHERE id = 2;
-DELETE FROM Developers WHERE id = 3;
-
--- Delete Sales data.
-DELETE FROM Sales WHERE id = 1;
-DELETE FROM Sales WHERE id = 2;
-DELETE FROM Sales WHERE id = 3;
-
--- Delete VideoGames data.
-DELETE FROM VideoGames WHERE id = 1;
-DELETE FROM VideoGames WHERE id = 2;
-DELETE FROM VideoGames WHERE id = 3;
 
 
 
