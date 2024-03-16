1e70bd54f8f4c4d90b7370aaf0a5c5410c326932
Jerome Cagado <128721661+jeromecagado@users.noreply.github.com>
2024-03-15 23:32:00 -0700

    Update db-connector.js
    For final project-no pw

--- database/db-connector.js
+++ database/db-connector.js
@@ -8,7 +8,7 @@ var pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'classmysql.engr.oregonstate.edu',
     user            : 'cs340_cagadoj',
-    password        : '3518',
+    password        : ''
     database        : 'cs340_cagadoj'
 })
 
